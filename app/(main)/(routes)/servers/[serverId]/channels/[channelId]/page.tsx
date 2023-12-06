4723454e6392ffcd110573dd1affafbe1921c489
ravindramohith <ravindramohith@gmail.com>
2023-12-06 14:44:14 +0530

    (#13) Done Chat Header and Mobile Toggle

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
-
-const ChannelIdPage = () => {
-  return (
-    <div>ChannelIdPage</div>
-  )
+import ChatHeader from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
+import React from "react";
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
 }
 
-export default ChannelIdPage
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: { id: params.channelId },
+  });
+
+  const member = await db.member.findFirst({
+    where: { profileId: profile.id, serverId: params.serverId },
+  });
+
+  if (!channel || !member) redirect(`/`);
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
+};
+
+export default ChannelIdPage;
