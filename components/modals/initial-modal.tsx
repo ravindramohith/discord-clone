b229a76b7b1647e3e819326f4d94a5bfc51b5834
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-16 16:09:16 +0530

    (#27) Minor UI Fix

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -76,7 +76,7 @@ export const InitialModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Customize your server
+            Create your first server
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
             Give your server a personality with a name and an image. You can always change it later.
