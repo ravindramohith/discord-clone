a996eddd9ef4cbd59beae2353631bb6ef5d383ac
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2024-08-09 17:48:42 +0530

    build & deploy to AWS

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -6,7 +6,7 @@ import { ChatHeader } from "@/components/chat/chat-header";
 import { db } from "@/lib/db";
 import ChatInput from "@/components/chat/chat-input";
 import ChatMessages from "@/components/chat/chat-messages";
-import { ChannelType } from "@prisma/client";
+import { ChannelType } from "./../node_modules/.prisma/client";
 import { MediaRoom } from "@/components/media-room";
 
 interface ChannelIdPageProps {
